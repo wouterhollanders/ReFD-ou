4ffec65f8815be4c95c017298e74f4552f1cd144
Wouter Hollanders <wouterhollanders@hotmail.com>
2024-12-10 09:06:37 +0100

    Form Template Method -part 1

--- src/nl/ou/refd/locations/graph/SelectionUtil.java
+++ src/nl/ou/refd/locations/graph/SelectionUtil.java
@@ -1,5 +1,9 @@
 package nl.ou.refd.locations.graph;
 
+import com.ensoftcorp.atlas.core.db.graph.Node;
+import com.ensoftcorp.atlas.core.db.set.AtlasSet;
+import com.ensoftcorp.atlas.core.query.Query;
+
 /**
  * Class which helps with selecting elements through utility methods.
  */
@@ -13,4 +17,24 @@ public class SelectionUtil {
 	public static GraphQuery getSelection() {
 		return new GraphQuery(com.ensoftcorp.atlas.ui.selection.SelectionUtil.getLastSelectionEvent().getSelection());
 	}
+	
+	
+	public static String getClassContent(String className) {
+        // Query all nodes tagged as "Class"
+        AtlasSet<Node> nodes = Query.universe()
+                .nodesTaggedWithAny("Class") // Find all class nodes
+                .eval().nodes(); // Evaluate and retrieve nodes
+
+        // Filter by name attribute
+        for (Node node : nodes) {
+            if (node.hasAttr("name") && node.getAttr("name").equals(className)) {
+                // Retrieve the "source" attribute or another attribute containing the code
+                if (node.hasAttr("source")) {
+                    return node.getAttr("source").toString();
+                }
+            }
+        }
+
+        return ""; // Return empty string if no match is found
+    }
 }
---
+++ src/nl/ou/refd/plugin/ui/CodeComparisonSelectionDialog.java
@@ -0,0 +1,88 @@
+package nl.ou.refd.plugin.ui;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.text.Document;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.TextSelection;
+import org.eclipse.jface.text.source.SourceViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.SashForm;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+
+public class CodeComparisonSelectionDialog extends Dialog {
+    private SourceViewer sourceViewer1;
+    private SourceViewer sourceViewer2;
+    private IDocument document1;
+    private IDocument document2;
+
+    public CodeComparisonSelectionDialog(Shell parentShell, String code1, String code2) {
+        super(parentShell);
+        this.document1 = new Document(code1);
+        this.document2 = new Document(code2);
+    }
+
+    @Override
+    protected Control createDialogArea(Composite parent) {
+        Composite container = (Composite) super.createDialogArea(parent);
+        container.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+
+        // Create a SashForm to divide the space proportionally
+        SashForm sashForm = new SashForm(container, SWT.HORIZONTAL);
+        sashForm.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+
+        // Editor 1
+        sourceViewer1 = createSourceViewer(sashForm, document1);
+
+        // Editor 2
+        sourceViewer2 = createSourceViewer(sashForm, document2);
+
+        // Set the weights for the sash form: 40% for each editor
+        sashForm.setWeights(new int[] { 40, 40 });
+
+        return container;
+    }
+
+    private SourceViewer createSourceViewer(Composite parent, IDocument document) {
+        SourceViewer sourceViewer = new SourceViewer(parent, null, SWT.BORDER | SWT.V_SCROLL | SWT.H_SCROLL);
+
+        // Set layout data to fill available space in the SashForm
+        sourceViewer.getTextWidget().setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+
+        // Set the document content
+        sourceViewer.setDocument(document);
+
+        // Make the text field read-only
+        sourceViewer.getTextWidget().setEditable(false);
+
+        return sourceViewer;
+    }
+
+    @Override
+    protected int getShellStyle() {
+        return super.getShellStyle() | SWT.RESIZE; // Add resizable style
+    }
+
+    @Override
+    protected void configureShell(Shell newShell) {
+        super.configureShell(newShell);
+        newShell.setText("Code Comparison");
+        newShell.setSize(800, 600); // Initial size
+        newShell.setMinimumSize(600, 400); // Optional: Minimum size
+    }
+
+    @Override
+    protected void okPressed() {
+        // Capture selections from both viewers
+        TextSelection selection1 = (TextSelection) sourceViewer1.getSelectionProvider().getSelection();
+        TextSelection selection2 = (TextSelection) sourceViewer2.getSelectionProvider().getSelection();
+
+        // Perform your refactoring logic using selection1 and selection2
+        System.out.println("Selection from Viewer 1: " + selection1.getText());
+        System.out.println("Selection from Viewer 2: " + selection2.getText());
+
+        super.okPressed();
+    }
+}
